46a5ec09b20b325ccf4cbc6cb3ccd978d1034c14
sary7373 <sarahchristen74@gmail.com>
2025-11-28 15:08:04 +0100

    aufgabe 2.3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -4,13 +4,21 @@ import "fmt"
 
 func main() {
 	// TODO: create a map called "modules"
+	modules := map[uint]string{
+		104: "Datenmodell implementieren",
+		117: "Informatik- und Netzinfrastruktur für ein kleines Unternehmen realisieren",
+		346: "Cloud Lösungen konzipieren und realisieren",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
 	// TODO: delete one
+	delete(modules, 104)
 	// TODO: add one
+	modules[164] = "Datenbanken erstellen und Daten einfügen"
 	// TODO: replace one
+	modules[117] = "Standortgebundene Kommunikationsdienste betreiben"
 	fmt.Println(modules)
 }
