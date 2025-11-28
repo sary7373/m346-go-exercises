e8bd5b51c6018d888cb5ff592ec843598b83dd28
sary7373 <sarahchristen74@gmail.com>
2025-11-28 14:42:32 +0100

    aufgabe 2.1

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -4,12 +4,21 @@ import "fmt"
 
 type FullName struct {
 	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
 // TODO: declare a structure for birth date
+type BirthDate struct {
+	BirthDay   byte
+	BirthMonth byte
+	BirthYear  uint16
+}
 
 type Profile struct {
 	// TODO: embed full name and birth date information
+	Name             FullName
+	BornOn           BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
@@ -17,12 +26,23 @@ type Profile struct {
 func main() {
 	var me = Profile{
 		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		Name: FullName{
+			"Sarah",
+			"Christen",
+		},
+		BornOn: BirthDate{
+			30,
+			8,
+			2009,
+		},
+		NumberOfSiblings: 1,        // TODO: adjust
+		ZodiacSign:       '\u264D', // TODO: adjust
 	}
-	fmt.Println(me)
+	fmt.Println(me)                   //printing struct like this doesn't work for zodiac sign emoji
+	fmt.Printf("%c\n", me.ZodiacSign) //but it works like that
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
 	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings += 1
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
