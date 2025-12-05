f19f1cb82c0bf6f1e9a6d31d9694194850c7bccf
sary7373 <sarahchristen74@gmail.com>
2025-12-05 15:07:48 +0100

    aufgabe 3.3

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 const (
 	Lower = 1
 	Upper = 30
@@ -7,4 +9,16 @@ const (
 
 func main() {
 	// TODO: Implement FizzBuzz using a for loop from Lower to Upper.
+	for i := Lower; i <= Upper; i++ {
+		fmt.Printf("%d%c ", i, '.')
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
