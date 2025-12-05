a31d48f77fa57c8fd1b1e617b490f61a60b82950
sary7373 <sarahchristen74@gmail.com>
2025-12-05 14:42:21 +0100

    aufgabe 3.2

--- go-3-ex-2/main.go
+++ go-3-ex-2/main.go
@@ -21,12 +21,33 @@ func outputDateRange(zodiacSign rune) {
 	fmt.Printf("%c: ", zodiacSign)
 	// TODO: Replace if, else if branching with switch/case.
 	// TODO: Define all 12 cases...
-	if zodiacSign == Aries {
-		fmt.Println("21.03. - 20.04")
-	} else if zodiacSign == Taurus {
-		fmt.Println("21.04. - 21.05")
-	} else {
-		fmt.Println("")
+	switch zodiacSign {
+	case Aries:
+		fmt.Println("21.03. - 20.04.")
+	case Taurus:
+		fmt.Println("21.04. - 21.05.")
+	case Gemini:
+		fmt.Println("22.05. - 21.06.")
+	case Cancer:
+		fmt.Println("22.06. - 22.07.")
+	case Leo:
+		fmt.Println("23.07. - 22.08.")
+	case Virgo:
+		fmt.Println("23.08. - 22.09.")
+	case Libra:
+		fmt.Println("23.09. - 22.10.")
+	case Scorpius:
+		fmt.Println("23.10. - 22.11.")
+	case Sagittarius:
+		fmt.Println("23.11. - 20.12.")
+	case Capricornus:
+		fmt.Println("21.12. - 19.01.")
+	case Aquarius:
+		fmt.Println("20.01. - 18.02.")
+	case Pisces:
+		fmt.Println("19.02. - 20.03.")
+	default:
+		fmt.Println("no valid zodiac sign")
 	}
 	// TODO: ...and consider a default case.
 }
@@ -35,4 +56,5 @@ func main() {
 	for zodiacSign := Aries; zodiacSign <= Pisces; zodiacSign++ {
 		outputDateRange(zodiacSign)
 	}
+	outputDateRange('\U0001F3A8')
 }
