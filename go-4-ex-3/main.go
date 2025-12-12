a48417eeebacda326cc6169cef3fb6689c6f995b
sary7373 <sarahchristen74@gmail.com>
2025-12-12 15:02:44 +0100

    aufgabe 4.3

--- go-4-ex-3/main.go
+++ go-4-ex-3/main.go
@@ -1,7 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // TODO: implement the function computeQuadraticFormula
+func computeQuadraticFormula(a float64, b float64, c float64) []float64 {
+	D := math.Pow(b, 2) - 4*a*c
+	if D < 0 {
+		return []float64{}
+	} else {
+		x1 := (-b + math.Sqrt(D)) / (2 * a)
+		if D == 0 {
+			return []float64{x1}
+		} else {
+			x2 := (-b - math.Sqrt(D)) / (2 * a)
+			return []float64{x1, x2}
+		}
+	}
+}
 
 func main() {
 	// TODO: call the function computeQuadraticFormula
+	fmt.Println(computeQuadraticFormula(3, 4, 1)) //-0.3333333333333333, -1
+	fmt.Println(computeQuadraticFormula(2, 4, 2)) //-1
+	fmt.Println(computeQuadraticFormula(3, 4, 2)) //leer
 }
