14c7e84594b0f253d7f7f7f1db0d58c5a93a8134
sary7373 <sarahchristen74@gmail.com>
2025-12-12 14:41:28 +0100

    aufgabe 4.1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,27 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 // TODO: implement the function computeGrade
+func computeGrade(gotPoints float32, maxPoints float32) (float32, error) {
+	grade := gotPoints/maxPoints*5 + 1
+	var err error
+	if gotPoints < 0 || maxPoints < 0 || gotPoints > maxPoints {
+		err = errors.New("invalid grade")
+	} else {
+		err = nil
+	}
+	return grade, err
+}
 
 func main() {
 	// TODO: call the function computeGrade
+	fmt.Println(computeGrade(17.5, 28.0))  //4.125
+	fmt.Println(computeGrade(67, 69))      //5.8550725
+	fmt.Println(computeGrade(8.25, 15.5))  //3.6612904
+	fmt.Println(computeGrade(-8.25, 15.5)) //invalid grade
+	fmt.Println(computeGrade(20, 15.5))    //invalid grade
 }
